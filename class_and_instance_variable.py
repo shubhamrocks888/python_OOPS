53d02d4130637c3ba509d40d39fb51d6d1b4acf2
unknown <shubhamrocks888@gmail.com>
2020-08-05 20:14:28 +0530

    class variable and instance variable

--- class_and_instance_variable.py
+++ class_and_instance_variable.py
@@ -1,71 +1,40 @@
+##                              Class or Static Variables in Python
 
-                        '''Class and Instance Variables (Or attributes)'''
-In Python, instance variables are variables whose value is assigned inside
-a constructor or method with self.
+The Python approach is simple, it doesn’t require a static keyword.
+All variables which are assigned a value in class declaration are class variables.
+And variables which are assigned values inside methods are instance variables.
 
-Class variables are variables whose value is assigned in class.
 
 # Python program to show that the variables with a value 
-# assigned in class declaration, are class variables and 
-# variables inside methods and constructors are instance 
-# variables. 
+# assigned in class declaration, are class variables 
 
 # Class for Computer Science Student 
 class CSStudent: 
-
-	# Class Variable 
-	stream = 'cse'			
-
-	# The init method or constructor 
-	def __init__(self, roll): 
-	
-		# Instance Variable	 
-		self.roll = roll	 
+	stream = 'cse'				 # Class or static Variable 
+	def __init__(self,name,roll): 
+		self.name = name		 # Instance or non-static Variable 
+		self.roll = roll		 # Instance or non-static Variable 
 
 # Objects of CSStudent class 
-a = CSStudent(101) 
-b = CSStudent(102) 
+a = CSStudent('Geek', 1) 
+b = CSStudent('Nerd', 2) 
 
 print(a.stream) # prints "cse" 
 print(b.stream) # prints "cse" 
-print(a.roll) # prints 101 
+print(a.name) # prints "Geek" 
+print(b.name) # prints "Nerd" 
+print(a.roll) # prints "1" 
+print(b.roll) # prints "2" 
 
 # Class variables can be accessed using class 
 # name also 
-print(CSStudent.stream) # prints "cse"	 
+print(CSStudent.stream) # prints "cse" 
 
 
 
-'''We can define instance variables inside normal methods also.'''
 
-# Python program to show that we can create 
-# instance variables inside methods 
 
-# Class for Computer Science Student 
-class CSStudent: 
-	
-	# Class Variable 
-	stream = 'cse'	
-	
-	# The init method or constructor 
-	def __init__(self, roll): 
-		
-		# Instance Variable 
-		self.roll = roll			 
 
-	# Adds an instance variable 
-	def setAddress(self, address): 
-		self.address = address 
-	
-	# Retrieves instance variable	 
-	def getAddress(self):	 
-		return self.address 
 
-# Driver Code 
-a = CSStudent(101) 
-a.setAddress("Noida, UP") 
-print(a.getAddress()) 
 
-##Output :
-Noida, UP
-
+ 
