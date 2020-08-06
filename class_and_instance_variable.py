f54724ce903ae9270f5ce0ca59c7feb6e373e0f1
unknown <shubhamrocks888@gmail.com>
2020-08-06 09:56:35 +0530

    changing class members

--- class_and_instance_variable.py
+++ class_and_instance_variable.py
@@ -31,7 +31,34 @@ print(b.roll) # prints "2"
 print(CSStudent.stream) # prints "cse" 
 
 
+##                                Changing Class Members in Python
 
+We should change class variables using class name only.
+
+# Program to show how to make changes to the 
+# class variable in Python 
+
+# Class for Computer Science Student 
+class CSStudent: 
+	stream = 'cse'	 # Class Variable 
+	def __init__(self, name, roll): 
+		self.name = name 
+		self.roll = roll 
+
+# New object for further implementation 
+a = CSStudent("check", 3) 
+print ("a.tream =", a.stream) 
+
+# Correct way to change the value of class variable 
+CSStudent.stream = "mec"
+print ("\nClass variable changes to mec")
+
+# New object for further implementation 
+b = CSStudent("carter", 4) 
+
+print ("\nValue of variable steam for each object")
+print ("a.stream =", a.stream) 
+print ("b.stream =", b.stream) 
 
 
 
